ef05c0235617877205f86b7889da0d5ad0293ebf
Edcko <edckoo1612@gmail.com>
2026-04-13 12:26:14 -0600

    fix: update hefesto v1.0.0 checksums (orchestrator delegation hardening)

--- Formula/hefesto.rb
+++ Formula/hefesto.rb
@@ -13,22 +13,22 @@ class Hefesto < Formula
   on_macos do
     on_arm do
       url "https://github.com/Edcko/Hefesto/releases/download/v#{version}/hefesto-darwin-arm64"
-      sha256 "aa17540187e186d1070212d3cac2ba882c0195fb8db8993b93ef3827d4c66056"
+      sha256 "e1a7d9005aa3d9af664a6ebde46208e5b0edce0f109b5ec120b199f4c1e4fdf3"
     end
     on_intel do
       url "https://github.com/Edcko/Hefesto/releases/download/v#{version}/hefesto-darwin-amd64"
-      sha256 "d999fe63d7933bc98e7ddaf33126aa72df12aea2d37f5a408b99e599e07c5c76"
+      sha256 "abb1c2ba03036e8dcca4178a0911cbb21790e845992b8afbd365268c7d80be6f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Edcko/Hefesto/releases/download/v#{version}/hefesto-linux-arm64"
-      sha256 "1edbb41b4b58e669386ce3bd16420d0deee2826541587bd558ba371c5b75cf12"
+      sha256 "88269fce348e0a3ac990814c6334e78eb3f4319d138bbbbf6cd87737c7e3e7f9"
     end
     on_intel do
       url "https://github.com/Edcko/Hefesto/releases/download/v#{version}/hefesto-linux-amd64"
-      sha256 "aae7c651166b42ff26679f716c9e5e48ce7a3d3cbd297f55f1a5d97e4c4f069d"
+      sha256 "4caae872ca03d9c0d7a87cca5820a84b0ef1de5cdf3a3bc6cf78e65e498836e1"
     end
   end
 
